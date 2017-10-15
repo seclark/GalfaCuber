825d56397e37eada124bd9f6579a59d20d2ba110
seclark <susanclark19@gmail.com>
2017-10-15 12:55:42 -0300

    test indexer

--- code/make_rht_cube.py
+++ code/make_rht_cube.py
@@ -34,10 +34,10 @@ cube_edgera1, cube_edgedec1 = cutouts.xy_to_radec(cube_edgex1, cube_edgey1, cube
 print(cube_edgera1, cube_edgedec1)
 
 allsky_w = cutouts.make_wcs(galfa_allsky_hdr)
-cutout_x1, cutout_y1 = cutouts.radec_to_xy(cube_edgera1, cube_edgedec1, allsky_w)
+cutout_x1, cutout_y1 = cutouts.radec_to_xy(cube_crval1, cube_crval2, allsky_w)
 print(cutout_x1, cutout_y1)
 
-xycut_hdr, xycut_data = cutouts.xycutout_data(big_data, big_hdr, xstart = 0, xstop = None, ystart = 0, ystop = None)
+#xycut_hdr, xycut_data = cutouts.xycutout_data(big_data, big_hdr, xstart = 0, xstop = None, ystart = 0, ystop = None)
 
 
 # construct a 2D header from galfa cube to project each theta slice to
