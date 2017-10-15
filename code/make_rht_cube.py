77f730ad5fd879af8213530adf7e4fefb766e8d8
seclark <susanclark19@gmail.com>
2017-10-15 13:28:35 -0300

    will round a bit

--- code/make_rht_cube.py
+++ code/make_rht_cube.py
@@ -29,16 +29,30 @@ cube_crval1 = galfa_cube_hdr['CRVAL1']
 cube_crval2 = galfa_cube_hdr['CRVAL2']
 cube_crpix1 = galfa_cube_hdr['CRPIX1']
 
+#test
 print(cube_crval1, cube_crval2)
 cube_w = cutouts.make_wcs(galfa_cube_fn)
 cube_edgex1, cube_edgey1 = cutouts.radec_to_xy(cube_crval1, cube_crval2, cube_w)
 print(cube_edgex1, cube_edgey1)
 cube_edgera1, cube_edgedec1 = cutouts.xy_to_radec(cube_edgex1, cube_edgey1, cube_w)
 print(cube_edgera1, cube_edgedec1)
-
 allsky_w = cutouts.make_wcs(galfa_allsky_hdr)
 cutout_x1, cutout_y1 = cutouts.radec_to_xy(cube_crval1, cube_crval2, allsky_w)
 print(cutout_x1, cutout_y1)
+
+# Actually grab cube edges
+cube_w = cutouts.make_wcs(galfa_cube_fn)
+cube_edgera1, cube_edgedec1 = cutouts.xy_to_radec(0, 0, cube_w)
+cube_edgera2, cube_edgedec2 = cutouts.xy_to_radec(galfa_cube_hdr["NAXIS1"], galfa_cube_hdr["NAXIS2"], cube_w)
+
+# translate cube corners to allsky x, y
+allsky_w = cutouts.make_wcs(galfa_allsky_hdr)
+cutout_x1, cutout_y1 = cutouts.radec_to_xy(cube_edgera1, cube_edgedec1, allsky_w)
+cutout_x2, cutout_y2 = cutouts.radec_to_xy(cube_edgera2, cube_edgedec2, allsky_w)
+
+# ROUND to integer x, y. Necessary because allsky header does not quite match up. Should check.
+print(cutout_x1, cutout_x2, cutout_y1, cutout_y2)
+cutout_xstart = np.int(cutout_x1)
 
 #xycut_hdr, xycut_data = cutouts.xycutout_data(big_data, big_hdr, xstart = 0, xstop = None, ystart = 0, ystop = None)
 
