db11bb163a90cac713d7d50a48d80af6e982bef1
seclark <susanclark19@gmail.com>
2017-11-13 08:00:30 -0500

    1064 to 1068

--- code/make_rht_cube.py
+++ code/make_rht_cube.py
@@ -280,7 +280,7 @@ if __name__ == "__main__":
 
     for ra in all_RAs:
         for dec in all_DECs:
-            make_single_cube_rtheta(RA=ra, DEC=dec, rht_velstart="1059", rht_velstop="1063", verbose=True)
+            make_single_cube_rtheta(RA=ra, DEC=dec, rht_velstart="1064", rht_velstop="1068", verbose=True)
             #make_single_cube_IQU(RA=ra, DEC=dec, verbose=True)
 
     #RA = "156.00"
