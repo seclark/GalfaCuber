1e6f3c1d5097f3ff21ca0961e0f360a392590f3d
seclark <susanclark19@gmail.com>
2017-11-14 14:48:49 -0500

    typo

--- code/extract_cube.py
+++ code/extract_cube.py
@@ -80,10 +80,10 @@ class NewCube():
             xmin, ymin = cutouts.radec_to_xy(self.RA_min, self.DEC_min, ppv_cube_wcs)
             xmax, ymax = cutouts.radec_to_xy(self.RA_max, self.DEC_max, ppv_cube_wcs)
             
-            xmin = max(np.ceil(xmin), np.naxis1)
+            xmin = max(np.ceil(xmin), self.naxis1)
             xmax = min(np.floor(xmax), 0)
             ymin = min(np.floor(ymin), 0)
-            ymax = max(np.ceil(ymax), np.naxis2)
+            ymax = max(np.ceil(ymax), self.naxis2)
             
             #if xmin > 0 and xmin < self.naxis1:
             #    print("xmin, naxis1", xmin, self.naxis1)
